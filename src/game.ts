cd5ce570b481d588df636bc0cb4afc1771d3ca4e
Edgar Nightingale <edgar.nightingale@geniussports.com>
2023-03-06 17:33:02 +0000

    Add goalkeeper

--- src/game.ts
+++ src/game.ts
@@ -3,12 +3,14 @@ import {Ball} from "./objects/ball";
 import {Button} from "./objects/button";
 import {Floor} from "./objects/floor";
 import {Goal} from "./objects/goal";
+import {Goalkeeper} from "./objects/goalkeeper";
 import {Player} from "./objects/player";
 
 export default class Demo extends Phaser.Scene {
 	player: Player;
 	ball: Ball;
 	goal: Goal;
+	goalkeeper: Goalkeeper;
 
 	constructor() {
 		super("demo");
@@ -18,6 +20,7 @@ export default class Demo extends Phaser.Scene {
 		this.load.image("ball", "assets/ball.png");
 		this.load.image("background", "assets/goal2.jpg");
 		this.load.image("button", "assets/button.png");
+		this.load.image("goalkeeper", "assets/goalkeeper.png");
 		this.load.audio("kick", "assets/sport_soccer_ball_kick.mp3");
 		this.load.audio("goal", "assets/human_crowd_approx_150_people_cheer_indoors.mp3");
 	}
@@ -26,6 +29,7 @@ export default class Demo extends Phaser.Scene {
 		const background = this.add.image(0, 0, "background").setOrigin(0);
 		const floor = new Floor(this, 400, 500, 0, 0, 800, 0);
 		this.goal = new Goal(this, 400, 240, 400, 140);
+		this.goalkeeper = new Goalkeeper(this, 400, 280, "goalkeeper");
 		this.ball = new Ball(this, 0, 300, "ball");
 		this.player = new Player(this, 400, 500, 50, 100);
 		const button = new Button(this, 400, 550, []);

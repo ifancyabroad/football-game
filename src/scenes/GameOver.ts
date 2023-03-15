56a5d9f072097bfa371ccb3b1ca3fa049612ce54
Edgar Nightingale <edgar.nightingale@geniussports.com>
2023-03-15 16:45:01 +0000

    Update game over screen

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -12,20 +12,24 @@ export class GameOver extends Phaser.Scene {
 
 		this.add.image(400, 300, "menu_background");
 		this.add
-			.text(400, 230, "CONGRATULATIONS!", {
+			.text(400, 210, "TIME UP!", {
 				fontSize: "24px",
 				fontFamily: "'Lilita One', cursive",
 				align: "center",
 			})
 			.setOrigin(0.5);
 		this.add
-			.text(400, 270, `You scored a total of ${data.score} goals.`, {
+			.text(400, 240, "You scored", {
 				fontSize: "16px",
 				fontFamily: "'Lilita One', cursive",
 				align: "center",
-				wordWrap: {
-					width: 200,
-				},
+			})
+			.setOrigin(0.5);
+		this.add
+			.text(400, 270, data.score.toString(), {
+				fontSize: "48px",
+				fontFamily: "'Lilita One', cursive",
+				align: "center",
 			})
 			.setOrigin(0.5);
 		this.button = new Button(this, "PLAY AGAIN", 400, 350);

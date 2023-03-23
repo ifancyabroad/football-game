5cb2efe7c3e7bdfaedd1a2bae8dfb813c1963411
Edgar Nightingale <edgar.nightingale@geniussports.com>
2023-03-23 15:26:16 +0000

    Add scaling

--- src/game.ts
+++ src/game.ts
@@ -3,8 +3,12 @@ import {Game, GameOver, Loading, Menu} from "./scenes";
 
 const config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	scale: {
+		parent: "game-container",
+		mode: Phaser.Scale.FIT,
+		width: 800,
+		height: 600,
+	},
 	physics: {
 		default: "arcade",
 		arcade: {
